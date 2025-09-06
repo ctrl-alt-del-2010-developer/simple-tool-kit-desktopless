fbeb869674e0f0336f2b99117f148ddbe49f9a1e
ctrl-alt-del-2010-developer <amongus.iso07@gmail.com>
2025-09-06 08:48:16 +0300

    Update setup.sh

--- STK/setup.sh
+++ STK/setup.sh
@@ -3,9 +3,9 @@ GREEN='\e[32m'
 YELLOW='\e[33m'
 RESET='\e[0m'
 
-# Detect package manager and install command
 detect_pkg_manager() {
-    if [ "$(uname)" = "Darwin" ]; then
+    OS=$(uname)
+    if [ "$OS" = "Darwin" ]; then
         PKG_MANAGER="brew"
         INSTALL_CMD="brew install"
         PY2="python@2"
@@ -14,33 +14,82 @@ detect_pkg_manager() {
     elif [ -f /etc/os-release ]; then
         . /etc/os-release
         case "$ID" in
-            ubuntu|debian)
+            ubuntu|debian|linuxmint|pop|elementary|zorin)
                 PKG_MANAGER="apt"
                 INSTALL_CMD="sudo apt install -y"
                 PY2="python2"
                 PY3="python3"
-                PIP="pip"
+                PIP="python3-pip"
+            ;;
+            kali)
+                PKG_MANAGER="apt"
+                INSTALL_CMD="sudo apt install -y"
+                PY2="python2"
+                PY3="python3"
+                PIP="python3-pip"
             ;;
             fedora)
                 PKG_MANAGER="dnf"
                 INSTALL_CMD="sudo dnf install -y"
                 PY2="python2"
                 PY3="python3"
-                PIP="pip"
+                PIP="python3-pip"
+            ;;
+            rhel|almalinux|rocky|centos)
+                PKG_MANAGER="yum"
+                INSTALL_CMD="sudo yum install -y"
+                PY2="python2"
+                PY3="python3"
+                PIP="python3-pip"
             ;;
             arch)
                 PKG_MANAGER="pacman"
                 INSTALL_CMD="sudo pacman -S --noconfirm"
                 PY2="python2"
                 PY3="python"
-                PIP="pip"
+                PIP="python-pip"
+            ;;
+            manjaro)
+                PKG_MANAGER="pacman"
+                INSTALL_CMD="sudo pacman -S --noconfirm"
+                PY2="python2"
+                PY3="python"
+                PIP="python-pip"
             ;;
             opensuse*|suse)
                 PKG_MANAGER="zypper"
                 INSTALL_CMD="sudo zypper install -y"
                 PY2="python2"
                 PY3="python3"
-                PIP="pip"
+                PIP="python3-pip"
+            ;;
+            solus)
+                PKG_MANAGER="eopkg"
+                INSTALL_CMD="sudo eopkg install -y"
+                PY2="python2"
+                PY3="python3"
+                PIP="python3-pip"
+            ;;
+            void)
+                PKG_MANAGER="xbps"
+                INSTALL_CMD="sudo xbps-install -Sy"
+                PY2="python2"
+                PY3="python3"
+                PIP="python3-pip"
+            ;;
+            gentoo)
+                PKG_MANAGER="emerge"
+                INSTALL_CMD="sudo emerge"
+                PY2="dev-lang/python:2.7"
+                PY3="dev-lang/python"
+                PIP="dev-python/pip"
+            ;;
+            clearlinux)
+                PKG_MANAGER="swupd"
+                INSTALL_CMD="sudo swupd bundle-add"
+                PY2="python2-basic"
+                PY3="python3-basic"
+                PIP="python3-pip"
             ;;
             *)
                 PKG_MANAGER="unknown"
@@ -51,26 +100,36 @@ detect_pkg_manager() {
     fi
 }
 
-clear 
+clear
 clear
 
 detect_pkg_manager
 
 if [ "$PKG_MANAGER" = "unknown" ]; then
-    echo -e "${RED}Unsupported operating system or package manager!${RESET}"
+    echo -e "${RED}Unsupported or unknown operating system!${RESET}"
     exit 1
 fi
 
-echo -e "${YELLOW}Installing packages with $PKG_MANAGER...${RESET}"
+echo -e "${YELLOW}Detected package manager: $PKG_MANAGER${RESET}"
 
 PACKAGES="$PY2 $PY3 $PIP cmatrix aircrack-ng iwd torbrowser-launcher sqlmap wireshark htop"
 
+# Check if the package manager is installed
+if ! command -v $PKG_MANAGER >/dev/null 2>&1; then
+    echo -e "${RED}$PKG_MANAGER is not installed or not in your PATH. Please install it and retry.${RESET}"
+    exit 2
+fi
+
+# Install packages
 $INSTALL_CMD $PACKAGES
 
-if [ "$PIP" = "pip3" ] || [ "$PY3" = "python3" ]; then
+# Install Python requirements
+if command -v pip3 >/dev/null 2>&1; then
     pip3 install -r requirements.txt --break-system-packages
-else
+elif command -v pip >/dev/null 2>&1; then
     pip install -r requirements.txt --break-system-packages
+else
+    echo -e "${RED}pip is not installed! Please install pip manually.${RESET}"
 fi
 
 clear
